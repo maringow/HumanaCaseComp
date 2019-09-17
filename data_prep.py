d993d5eb0b678fc4c43d05c68c7d5c245ac6f476
maringow <mgow@westmonroepartners.com>
2019-09-17 10:08:36 -0500

    calculated LTOT for first 180 days

--- data_prep.py
+++ data_prep.py
@@ -9,17 +9,32 @@ import numpy as np
 pd.set_option('display.max_columns', None)
 
 # read in data
-raw_df = pd.read_csv('C:\\Users\\mgow\\Documents\\Personal\\Humana Case Comp\\HMAHCC_COMP.csv')
-print(raw_df.head(n=10))
+df_raw = pd.read_csv('C:\\Users\\mgow\\Documents\\Personal\\Humana Case Comp\\HMAHCC_COMP.csv')
+print(df_raw.head(n=10))
 
-# create dataframe at member level
-df = raw_df.groupby('id')['event_descr'].count()
-print(df.head(n=10))
-print(len(df))
+df = pd.DataFrame(columns=['id', 'count_leadup_events', 'ltot_status'])
+df['id'] = df_raw['id'].unique()
 
-# calculate and append LTOT status
-for row in df:
+# count how many events each member had before Day 0
+df['count_leadup_events'] = df['id'].map(df_raw[df_raw['Days'] < 0]['id'].value_counts())
 
+# create temporary dataframe of 180-day opioid supply for LTOT calculation and merge into df
+#TODO make this extensibly to 180-day rolling window
+df_supply_first_180 = df_raw[(df_raw['Days'] >= 0) & (df_raw['Days'] < 180)][['id', 'PAY_DAY_SUPPLY_CNT']]\
+    .groupby(['id']).sum()
+
+df = df.merge(
+    df_supply_first_180,
+    on='id',
+    how='left'
+)
+
+df = df.rename(columns={'PAY_DAY_SUPPLY_CNT': 'supply_first_180'})
+
+# set LTOT status based on opioid supply count
+df['ltot_status'] = df['supply_first_180'].apply(lambda x: 1 if x >= 162 else 0)
+
+print(df.head(100))
 
 
 
