642e8d8e210023b0e392af2b4368c10a5d6d8000
maringow <mgow@westmonroepartners.com>
2019-09-22 13:31:33 -0500

    started qualifying event and coverage calcs

--- data_prep.py
+++ data_prep.py
@@ -12,30 +12,55 @@ pd.set_option('display.max_columns', None)
 df_raw = pd.read_csv('C:\\Users\\mgow\\Documents\\Personal\\Humana Case Comp\\HMAHCC_COMP.csv')
 print(df_raw.head(n=10))
 
-df = pd.DataFrame(columns=['id', 'count_leadup_events', 'ltot_status'])
-df['id'] = df_raw['id'].unique()
+# build outcomes dataframe - at member level, to be populated with LTOT outcome
+df_outcomes = pd.DataFrame(columns=['id', 'count_leadup_events', 'ltot_status'])
+df_outcomes['id'] = df_raw['id'].unique()
 
-# count how many events each member had before Day 0
-df['count_leadup_events'] = df['id'].map(df_raw[df_raw['Days'] < 0]['id'].value_counts())
+df_outcomes['count_leadup_events'] = df_outcomes['id']\
+    .map(df_raw[df_raw['Days'] < 0]['id'].value_counts())
 
-# create temporary dataframe of 180-day opioid supply for LTOT calculation and merge into df
-#TODO make this extensibly to 180-day rolling window
-df_supply_first_180 = df_raw[(df_raw['Days'] >= 0) & (df_raw['Days'] < 180)][['id', 'PAY_DAY_SUPPLY_CNT']]\
-    .groupby(['id']).sum()
+df_outcomes['ltot_status'] = 0
 
-df = df.merge(
-    df_supply_first_180,
-    on='id',
-    how='left'
-)
+df_outcomes.head(10)
 
-df = df.rename(columns={'PAY_DAY_SUPPLY_CNT': 'supply_first_180'})
+# create dataframe of all opioid prescriptions
+df_opioid = df_raw[df_raw['event_descr'] == 'RX Claim - Paid']
+df_opioid = df_opioid[df_opioid['MME'].isnull() == False]
+df_opioid = df_opioid.rename(columns={'id': 'member_id'})
 
-# set LTOT status based on opioid supply count
-df['ltot_status'] = df['supply_first_180'].apply(lambda x: 1 if x >= 162 else 0)
+df_opioid.head()
 
-print(df.head(100))
+# select only members that total at least 163 days of opioids supplied
+df_total_opioid_counts = df_raw[['id', 'PAY_DAY_SUPPLY_CNT']].groupby('id').sum()
+possible_positives = df_total_opioid_counts[df_total_opioid_counts['PAY_DAY_SUPPLY_CNT'] >= 162].index
 
+len(possible_positives)
+
+# build shell dataframe of member id, day, coverage, and qualifying events for days 0 onward
+
+day_range = range(-1300, 1101)
+index = pd.MultiIndex.from_product([possible_positives, day_range], names = ['member_id', 'day'])
+df_coverage = pd.DataFrame(index=index).reset_index()
+df_coverage['covered'] = 0
+df_coverage['qe'] = 0
+
+df_coverage.head()
+
+# distribute opioid pay day supply counts across df_coverage. Day prescribed is day 1
+
+df_opioid = df_opioid[df_opioid['member_id'] == 'ID10013863216']  # test with 1 ID - remove later
+df_opioid.head(50)
+
+for i in df_opioid.index:
+    day = df_opioid['Days'][i]
+    member_id = df_opioid['member_id'][i]
+    count = df_opioid['PAY_DAY_SUPPLY_CNT'][i]
+    while count > 0:
+        df_coverage[(df_coverage['day'] == day) & (df_coverage['member_id'] == member_id)]['covered'] = 1
+        day += 1
+        count = count - 1
+
+df_coverage.head()
 
 
 #####################################################################################
