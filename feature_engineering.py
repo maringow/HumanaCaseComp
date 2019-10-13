71e57c422a6e8e210c7410a2aadecd9922c68e83
maringow <mgow@westmonroepartners.com>
2019-10-13 16:42:37 -0500

    adjusted code for holdout file

--- feature_engineering.py
+++ feature_engineering.py
@@ -7,9 +7,10 @@ import data_prep
 #####################################################################################
 
 # read in data
-df_outcomes = pd.read_csv('df_outcomes.csv')
-df_raw = pd.read_csv('C:\\Users\\mgow\\Documents\\Personal\\Humana Case Comp\\HMAHCC_COMP.csv')
-
+# df_outcomes = pd.read_csv('df_outcomes.csv')
+df_holdout_outcomes = pd.read_csv('HOLDOUT_outcomes.csv')
+# df_raw = pd.read_csv('C:\\Users\\mgow\\Documents\\Personal\\Humana Case Comp\\HMAHCC_COMP.csv')
+df_holdout_raw = pd.read_csv('C:\\Users\\mgow\\Documents\\Personal\\Humana Case Comp\\HMAHCC_HOLDOUT.csv')
 
 # days before 0 - used to standardize
 '''
@@ -22,94 +23,97 @@ group by id
 order by count(distinct event_attr5) desc
 '''
 
-df_days_before_0 = df_raw[['id', 'Days']].groupby('id').min().reset_index()
-df_days_before_0 = df_days_before_0.rename(columns={'Days': 'days_before_0'})
-df_days_before_0['days_before_0'] = abs(df_days_before_0['days_before_0'])
+def add_features(df_raw, df_outcomes):
 
-df_outcomes = df_outcomes.merge(df_days_before_0, how = 'left', left_on = 'member_id', right_on = 'id', right_index=False)
-df_outcomes.drop(columns='id', inplace=True)
+    df_days_before_0 = df_raw[['ID', 'days']].groupby('ID').min().reset_index()
+    df_days_before_0 = df_days_before_0.rename(columns={'days': 'days_before_0'})
+    df_days_before_0['days_before_0'] = abs(df_days_before_0['days_before_0'])
 
-# Total number of different prescriptions that they’ve had prior to day 0 / days before 0
-df_prescriptions = df_raw.loc[(df_raw.event_descr == 'RX Claim - Paid') & (df_raw.Days < 0)]
-df_prescriptions.rename(columns={'id': 'member_id'}, inplace=True)
+    df_outcomes = df_outcomes.merge(df_days_before_0, how = 'left', left_on = 'member_id', right_on = 'ID', right_index=False)
+    df_outcomes.drop(columns='ID', inplace=True)
 
-df_unique_prescriptions = df_prescriptions[['member_id', 'event_attr5']].groupby('member_id').nunique()
-df_unique_prescriptions.drop(columns='member_id', inplace=True)
-df_unique_prescriptions.rename(columns={'event_attr5': 'count_unique_scripts'}, inplace=True)
+    # Total number of different prescriptions that they’ve had prior to day 0 / days before 0
+    df_prescriptions = df_raw.loc[(df_raw.EVENT_DESCR == 'RX Claim - Paid') & (df_raw.days < 0)]
+    df_prescriptions.rename(columns={'ID': 'member_id'}, inplace=True)
 
-df_outcomes = df_outcomes.merge(df_unique_prescriptions, how = 'left', on = 'member_id', right_index=False)
+    df_unique_prescriptions = df_prescriptions[['member_id', 'EVENT_ATTR5']].groupby('member_id').nunique()
+    df_unique_prescriptions.drop(columns='member_id', inplace=True)
+    df_unique_prescriptions.rename(columns={'EVENT_ATTR5': 'count_unique_scripts'}, inplace=True)
 
-df_outcomes['count_unique_scripts_std'] = df_outcomes['count_unique_scripts'] / df_outcomes['days_before_0']
+    df_outcomes = df_outcomes.merge(df_unique_prescriptions, how = 'left', on = 'member_id', right_index=False)
 
-# Supply sum of opioids before day 0 / number of days before 0
+    df_outcomes['count_unique_scripts_std'] = df_outcomes['count_unique_scripts'] / df_outcomes['days_before_0']
 
-'''
-QUERY FOR TESTING:
+    # Supply sum of opioids before day 0 / number of days before 0
 
-select id, sum(cast(pay_day_supply_cnt as int)) from comp
-where event_descr = 'RX Claim - Paid'
-and MME is not null
-and Days < 0
-group by id
-'''
+    '''
+    QUERY FOR TESTING:
+    
+    select id, sum(cast(pay_day_supply_cnt as int)) from comp
+    where event_descr = 'RX Claim - Paid'
+    and MME is not null
+    and Days < 0
+    group by id
+    '''
 
-df_opioid_prescriptions = df_raw.loc[(df_raw.event_descr == 'RX Claim - Paid') & (df_raw.Days < 0) & (df_raw.MME.isnull() == False)]
-df_opioid_prescriptions.rename(columns={'id': 'member_id'}, inplace=True)
+    df_opioid_prescriptions = df_raw.loc[(df_raw.EVENT_DESCR == 'RX Claim - Paid') & (df_raw.days < 0) & (df_raw.MME.isnull() == False)]
+    df_opioid_prescriptions.rename(columns={'ID': 'member_id'}, inplace=True)
 
-df_opioid_sum = df_prescriptions[['member_id', 'PAY_DAY_SUPPLY_CNT']].groupby('member_id').sum().reset_index()
-df_opioid_sum.rename(columns={'PAY_DAY_SUPPLY_CNT': 'sum_prior_opioids'}, inplace=True)
+    df_opioid_sum = df_prescriptions[['member_id', 'PAY_DAY_SUPPLY_CNT']].groupby('member_id').sum().reset_index()
+    df_opioid_sum.rename(columns={'PAY_DAY_SUPPLY_CNT': 'sum_prior_opioids'}, inplace=True)
 
-df_outcomes = df_outcomes.merge(df_opioid_sum, how = 'left', on = 'member_id', right_index=False)
-df_outcomes['sum_prior_opioids_std'] = df_outcomes['sum_prior_opioids'] / df_outcomes['days_before_0']
+    df_outcomes = df_outcomes.merge(df_opioid_sum, how = 'left', on = 'member_id', right_index=False)
+    df_outcomes['sum_prior_opioids_std'] = df_outcomes['sum_prior_opioids'] / df_outcomes['days_before_0']
 
-# Flag if they took opioids before day 0
-df_outcomes['prior_opioids_flag'] = df_outcomes['sum_prior_opioids'].apply(lambda x: 1 if x > 0 else 0)
+    # Flag if they took opioids before day 0
+    df_outcomes['prior_opioids_flag'] = df_outcomes['sum_prior_opioids'].apply(lambda x: 1 if x > 0 else 0)
+
+    # flag if member has had a prior psych prescription
+
+    df_psych_prescriptions = df_raw.loc[(df_raw.EVENT_DESCR == 'RX Claim - Paid') & (df_raw.days < 0) & (df_raw.EVENT_ATTR6.str.contains('PSYCH'))]
+    df_psych_prescriptions.rename(columns={'ID': 'member_id'}, inplace=True)
+
+    prior_psych_members = df_psych_prescriptions['member_id'].unique()
+
+    df_outcomes['prior_psych_flag'] = df_outcomes['member_id'].apply(lambda x: 1 if x in prior_psych_members else 0)
+
+    # flag if member has had a prior pain prescription
+
+    df_pain_prescriptions = df_raw.loc[(df_raw.EVENT_DESCR == 'RX Claim - Paid') & (df_raw.days < 0) & (df_raw.EVENT_ATTR6 == 'PAIN')]
+    df_pain_prescriptions.rename(columns={'ID': 'member_id'}, inplace=True)
+
+    prior_pain_members = df_pain_prescriptions['member_id'].unique()
+
+    df_outcomes['prior_pain_flag'] = df_outcomes['member_id'].apply(lambda x: 1 if x in prior_pain_members else 0)
+
+    # remove rows that should not be included in model
+    df_outcomes.drop(columns='X', inplace=True)
+    df_outcomes.set_index('member_id', inplace=True)
+    # unqualified_members = ['ID13362382498',
+    #                         'ID14698922966',
+    #                         'ID19738321946',
+    #                         'ID21430468895',
+    #                         'ID27220994153',
+    #                         'ID36354083119',
+    #                         'ID55796310983',
+    #                         'ID59083521579',
+    #                         'ID62579261726',
+    #                         'ID63293388003',
+    #                         'ID77890885331',
+    #                         'ID86806699161',
+    #                         'ID96200812839',
+    #                         'ID98715617553']
+
+    # df_outcomes.drop(unqualified_members, inplace=True)
+
+    df_outcomes.dropna(axis=0, subset=['Day0MME'], inplace=True)
+    return df_outcomes
 
 
-# flag if member has had a prior psych prescription
-
-df_psych_prescriptions = df_raw.loc[(df_raw.event_descr == 'RX Claim - Paid') & (df_raw.Days < 0) & (df_raw.event_attr6.str.contains('PSYCH'))]
-df_psych_prescriptions.rename(columns={'id': 'member_id'}, inplace=True)
-
-prior_psych_members = df_psych_prescriptions['member_id'].unique()
-
-df_outcomes['prior_psych_flag'] = df_outcomes['member_id'].apply(lambda x: 1 if x in prior_psych_members else 0)
+df_comp_outcomes = add_features(df_holdout_raw, df_holdout_outcomes)
+df_comp_outcomes.to_csv('df_comp_outcomes.csv')
 
 
-# flag if member has had a prior pain prescription
-
-df_pain_prescriptions = df_raw.loc[(df_raw.event_descr == 'RX Claim - Paid') & (df_raw.Days < 0) & (df_raw.event_attr6 == 'PAIN')]
-df_pain_prescriptions.rename(columns={'id': 'member_id'}, inplace=True)
-
-prior_pain_members = df_pain_prescriptions['member_id'].unique()
-
-df_outcomes['prior_pain_flag'] = df_outcomes['member_id'].apply(lambda x: 1 if x in prior_pain_members else 0)
-
-
-# remove rows that should not be included in model
-df_outcomes.drop(columns='X', inplace=True)
-df_outcomes.set_index('member_id', inplace=True)
-# unqualified_members = ['ID13362382498',
-#                         'ID14698922966',
-#                         'ID19738321946',
-#                         'ID21430468895',
-#                         'ID27220994153',
-#                         'ID36354083119',
-#                         'ID55796310983',
-#                         'ID59083521579',
-#                         'ID62579261726',
-#                         'ID63293388003',
-#                         'ID77890885331',
-#                         'ID86806699161',
-#                         'ID96200812839',
-#                         'ID98715617553']
-
-
-
-# df_outcomes.drop(unqualified_members, inplace=True)
-df_outcomes.dropna(axis=0, subset=['Day0MME'], inplace=True)
-
-df_outcomes.to_csv('df_outcomes_features.csv')
 
 
 
--- model_evaluation.py
+++ model_evaluation.py
@@ -6,9 +6,6 @@ from sklearn.model_selection import train_test_split
 pd.set_option('display.max_columns', None)
 df = pd.read_csv('df_outcomes_features.csv')
 
-df.drop(columns=['Unnamed: 0', 'index', 'X'], inplace=True)
-df.set_index('member_id', inplace=True)
-
 df_x = df.drop(columns='ltot_status')
 print(df_x.head())
 df_y = df['ltot_status']
@@ -16,6 +13,6 @@ print(df_y.head())
 
 # train-test split
 
-x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=.2, shuffle=True)
+x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=.25, shuffle=True)
 
 
